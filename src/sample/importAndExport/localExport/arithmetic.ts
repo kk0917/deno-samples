44d385808752657dd05731dc3212ada32ad0b40c
ko-kamenashi <kamenashi0917@gmail.com>
2020-12-22 20:52:36 +0900

    Add sample
    Example 6.2 Import and export modules
    https://deno.land/manual@v1.6.1/examples/import_export

---
+++ src/sample/importAndExport/localExport/arithmetic.ts
@@ -0,0 +1,7 @@
+export function add(outbound: number, inbound: number): number {
+  return outbound + inbound
+}
+
+export function multiply(sum: number, tax: number): number {
+  return sum * tax
+}
