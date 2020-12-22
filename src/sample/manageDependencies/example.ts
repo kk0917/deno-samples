c601a9db34fc1237f16e861b01ac7a36dd7bc335
ko-kamenashi <kamenashi0917@gmail.com>
2020-12-22 20:54:06 +0900

    Add sample
    Example 6.3 Manage dependencies
    https://deno.land/manual@v1.6.1/examples/manage_dependencies

---
+++ src/sample/manageDependencies/example.ts
@@ -0,0 +1,14 @@
+import { add, multiply } from "./deps.ts";
+
+function totalCost(outbound: number, inbound: number, tax: number): number {
+  return multiply(add(outbound, inbound), tax);
+}
+
+console.log(totalCost(19, 31, 1.2));
+console.log(totalCost(45, 27, 1.15));
+/**
+ * Output
+ *
+ * 60
+ * 82.8
+ */
