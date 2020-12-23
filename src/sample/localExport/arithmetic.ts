f61c4cf9ac2b40ed447290e3082d2e79e6b46863
ko-kamenashi <kamenashi0917@gmail.com>
2020-12-23 19:31:19 +0900

    chore: rm unnecessary files

--- src/sample/localExport/arithmetic.ts
+++
@@ -1,7 +0,0 @@
-export function add(outbound: number, inbound: number): number {
-  return outbound + inbound
-}
-
-export function multiply(sum: number, tax: number): number {
-  return sum * tax
-}
