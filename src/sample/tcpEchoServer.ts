c1aae29b75f85dd124cb8088e91573c07552af92
ko-kamenashi <kamenashi0917@gmail.com>
2020-12-25 08:44:39 +0900

    add sample
    Example 6.9 TCP echo server
    https://deno.land/manual@v1.6.1/examples/tcp_echo

---
+++ src/sample/tcpEchoServer.ts
@@ -0,0 +1,12 @@
+const listener = Deno.listen({port: 8080});
+
+console.log('Listening on 0.0.0.0:8080');
+
+for await (const conn of listener) {
+  Deno.copy(conn, conn);
+}
+/**
+ * ❯ deno run --allow-net src/sample/tcpEchoServer.ts
+ * Check file:///Users/ko-kamenashi/Desktop/Samples/Deno/deno-sample/src/sample/tcpEchoServer.ts
+ * Listening on 0.0.0.0:8080
+ */
